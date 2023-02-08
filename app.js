45f6d41ea8021dfba0de2abfa59b105a2d3a8ce6
thuanphatt <zelorntp@gmail.com>
2023-02-08 22:29:09 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +12,17 @@ app.get("/", (req, res) => {
   });
 });
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
